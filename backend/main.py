deeb883e9a5ccd75f9ac3496d53551dad418f197
francescocorradini30 <francescocorradini30g@gmail.com>
2026-04-18 19:16:50 +0200

    create party route

--- backend/main.py
+++ backend/main.py
@@ -4,6 +4,8 @@ from fastapi.templating import Jinja2Templates
 
 from fastapi.responses import HTMLResponse
 
+from fastapi import Form
+
 app = FastAPI()
 
 templates = Jinja2Templates(directory="frontend/templates")
@@ -18,4 +20,23 @@ def home(request: Request):
 
 @app.get("/create_party", response_class=HTMLResponse)
 async def create_party_page(request: Request):
-    return templates.TemplateResponse("", {"request": request})
+    return templates.TemplateResponse("create_party.html", {"request": request})
+
+@app.post("/create_party")
+async def create_party(
+    request: Request,
+    party_name: str = Form(...),
+    username: str = Form(...)
+):
+    # Debug print (always do this in early stage)
+    print(f"Party: {party_name}")
+    print(f"User: {username}")
+
+    return templates.TemplateResponse(
+        "create_party.html",
+        {
+            "request": request,
+            "party_name": party_name,
+            "username": username
+        }
+    )
